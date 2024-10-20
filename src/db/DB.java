0e8d888f0d4c582bf09bcd4821520bc15f575c5c
Lucas Monteiro <lucasmmnm@gmail.com>
2024-10-20 11:19:02 -0300

    Creating project and git repository

---
+++ src/db/DB.java
@@ -0,0 +1,76 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	// CLASSE DE CONEXÃO COM O BANCO
+	private static Connection conn = null;
+
+	// MÉTODO PARA CONECTAR COM O BANCO DE DADOS
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	// MÉTODO PARA FECHAR A CONEXÃO COM O BANCO DE DADOS
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			} 
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	// CÓDIGO PARA ACESSAR O PROPERTIES E ACESSAR AS VARIÁVEIS
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
